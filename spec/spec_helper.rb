fae16a47c6f28ec583bef897f256c85606301bf2
Kha Le & Andrew Lee <student@epicodus.com>
2015-05-07 16:00:03 -0700

    passes patrons specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require('books')
 require('rspec')
 require('pry')
 require('pg')
+require('patrons')
 
 DB = PG.connect({:dbname => 'library_system_test'})
 
@@ -10,5 +11,6 @@ RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM authors *;")
     DB.exec("DELETE FROM books *;")
+    DB.exec("DELETE FROM patrons *;")
   end
 end
